4f4c0e93b2b312860945c66ec5618f5f1c462f23
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-06 17:01:45 -0500

    finished server notes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,24 @@ const port = 5000;
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
+const quotesData = require ('./modules/quotes.js');
+
+let index = 0;
+
+app.get('/quotes', (req, res) => {
+    console.log('hi from get request');
+    res.send(quotesData[index]);
+});
+
+app.get('/randomQuotes', (req, res) => {
+    let randomNumber = getRandomInt(quotesData.length);
+    res.send(quotesData[randomNumber]);
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
 app.listen(port, () => {
     console.log("Up and running on port: ",port);
 });
